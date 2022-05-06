9f7fd5554bcba5b215ad41bc0d71b553fbd5e73e
Fraanco17 <96310401+Fraanco17@users.noreply.github.com>
2022-05-06 10:06:13 +0200

    Add files via upload

---
+++ CodigoProyecto/src/main/java/org/example/LuminosidadSAREF4BLDG.java
@@ -0,0 +1,9 @@
+package org.example;
+
+
+public class LuminosidadSAREF4BLDG extends Sensores{
+
+    public LuminosidadSAREF4BLDG(String fabricante, Integer numSerie, Integer fecha, String usuario) {
+        super(fabricante, numSerie, fecha, usuario);
+    }
+}
